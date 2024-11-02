d4ab61207d47d6bc75d89cec76a1a02179e1b55f
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-11-02 13:16:42 +0530

    ' replaced with &apos;

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -29,7 +29,7 @@ export const HeroSection = () => {
         <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
           <span>👋</span>
           <span className="font-semibold ">
-            let's connect
+            let&apos;s connect
           </span>
         </button>
       </div>
